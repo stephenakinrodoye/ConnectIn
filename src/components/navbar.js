37cc81749de52595cd24ac3cc821a216fc0e14cf
Stephen Akinrodoye <stephenakinrodoye2@gmail.com>
2024-09-23 20:44:18 +0100

    navbar completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,14 @@
-import "../styles/navbar.css"
 
 function Navbar() {
   const navLists = [
     {
-      icon: <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 24 24" data-supported-dps="24x24" 
-        fill="currentColor" 
-        class="mercado-match" 
-        width="24" 
-        height="24" 
-        focusable="false">
-        <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
+      icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+      <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
       </svg>,
       tag: "Home"
     },
     {
-      icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+      icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24"  fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
       <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
       </svg>,
       tag: "My Network"
@@ -41,53 +33,55 @@ function Navbar() {
     }
   ]
   return (
-    <div className="nav-container">
+    <div className="nav-container relative flex flex-row flex-nowrap justify-center bg-white border-b border-solid border-gray-300">
       {/*left side of the navbar */}
-      <div className="nav-container-left">
-        <svg className="nav-icon" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style={{ fill: "#0a66c2", transform: ";msFilter:;" }}><path d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM8.339 18.337H5.667v-8.59h2.672v8.59zM7.003 8.574a1.548 1.548 0 1 1 0-3.096 1.548 1.548 0 0 1 0 3.096zm11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387 2.704 0 3.203 1.778 3.203 4.092v4.71z"></path></svg>
+      <div className="nav-container-left relative flex flex-row flex-nowrap items-center gap-1 w-3/12 py-0 px-1">
+        <svg className="nav-icon rounded-lg min-w-[48px] h-12" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style={{ fill: "#0a66c2", transform: ";msFilter:;" }}><path d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM8.339 18.337H5.667v-8.59h2.672v8.59zM7.003 8.574a1.548 1.548 0 1 1 0-3.096 1.548 1.548 0 0 1 0 3.096zm11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387 2.704 0 3.203 1.778 3.203 4.092v4.71z"></path></svg>
 
-        <span className="nav-search">
+        <span className="nav-search flex justify-start items-center bg-[#edf3f8] p-[2px_16px] rounded-md w-1/2 transition-all duration-200 ease-linear focus-within:border-2 focus-within:border-black focus-within:w-full">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" style={{ fill: "rgba(0, 0, 0, 1)", transform: ";msFilter:;" }}><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path></svg>
-          <input type="search" placeholder="Search" />
+          <input type="search" placeholder="Search" className="border-0 outline-none p-[8px_10px] bg-transparent text-[15px] focus:w-full" />
         </span>
       </div>
 
       {/* The center of the navbar */}
       <div className="nav-container-center">
-        <nav className="navbar-items">
+        <nav className="navbar-items flex flex-row flex-nowrap">
           {
             navLists.map((eachNavList, index) => (
-              <a href="/" key={index}>
+              <a href="/" key={index} className="text-[12px] text-black h-full no-underline p-[6px_16px] opacity-60 transition-opacity duration-100 ease-in hover:opacity-100 first:opacity-100 first:border-b-2 first:border-black">
                 {eachNavList.icon}<div>{eachNavList.tag}</div>
               </a>
             ))
           }
-          <a>
+          <a className="text-[12px] text-black h-full no-underline p-[6px_16px] opacity-60 transition-opacity duration-100 ease-in hover:opacity-100">
             <img 
               src="https://api.dicebear.com/9.x/adventurer/svg?seed=Ryan"
               alt="avatar"
-              width={24}
-              height={24}
-              style={{ background: "navajowhite", borderRadius: "50%" }}
+              className="w-6 h-6 bg-[#ffdead] rounded-full"             
             />
             <div>Me</div>
           </a>
         </nav>
       </div>
 
-      <div className="nav-container-right">
-        <div className="for-business-container">
+      {/* Right */}
+      <div className="nav-container-right flex items-center border-l border-lightgray p-4 gap-6">
+        <div className="for-business-container flex items-center text-[12px] gap-1">
           <svg 
+            className="w-6 h-6"
             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match"  width="24" height="24" focusable="false">
             <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
           </svg> 
-          <div className="for-business-text">
+          <div className="for-business-text text-[12px] flex items-center gap-1">
             For business
-            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" style={{ fill: "rgba(0, 0, 0, .6)", transform: ";msFilter:;" }}><path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"></path></svg>
-          </div>         
+            <svg 
+            className="w-3 h-3 fill-current text-gray-500"
+            xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" style={{ fill: "rgba(0, 0, 0, .6)", transform: ";msFilter:;" }}><path d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"></path></svg>
+          </div>
         </div>
-        <div className="premium-container">
-          <span className="orange-box"></span>
+        <div className="premium-container text-[12px] flex items-center gap-1 text-left">
+          <span className="orange-box bg-gradient-to-br from-orange-400 to-[#f9c982] w-5 h-5 rounded-md"></span>
           <span>Try for NGN0: <br /> Premium</span>
         </div>
       </div>      
